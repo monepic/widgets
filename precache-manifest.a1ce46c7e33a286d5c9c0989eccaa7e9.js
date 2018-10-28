8fb58f9378dd39920ffbc54c59dca955276dd741
Ed <ed@monepic.com>
2018-10-28 19:43:32 +0000

    Updates

---
+++ precache-manifest.a1ce46c7e33a286d5c9c0989eccaa7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa5ea299e1e2b238224a",
+    "url": "/static/js/main.aa5ea299.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "aa5ea299e1e2b238224a",
+    "url": "/static/css/main.69011467.chunk.css"
+  },
+  {
+    "revision": "e934e3cc4b76a60d88bb81488c7137c5",
+    "url": "/index.html"
+  }
+];
